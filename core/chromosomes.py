9d803a824aeca480736ec36bdd93367dc2ba32bd
hunter-heidenreich <hunter.scott.heidenreich@gmail.com>
2018-12-25 12:33:40 -0500

    Add basic node mutation methods

--- core/chromosomes.py
+++ core/chromosomes.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+import numpy as np
+
 
 class Node(ABC):
     """
@@ -9,7 +11,42 @@ class Node(ABC):
     In a blueprint chromosome, this would represent which
     module is being pointed to.
     """
-    pass
+
+    HISTORICAL_MARKER = 1
+
+    def __init__(self):
+        # Save what this Node should be marked as
+        self._marker = Node.HISTORICAL_MARKER
+
+        # Update the marker count
+        Node.HISTORICAL_MARKER += 1
+
+    def _bit_flip_mutation(self, value):
+        """
+        A simple Node mutation that just flips whichever
+        boolean value to the opposite
+        :param value: The value to be flipped, bool
+        :return: The new value, bool
+        """
+        return not value
+
+    def _gaussian_mutation(self, mean=0.0, std_dev=1.0):
+        """
+        Draws a real-valued number from a Gaussian distribution
+        :param mean: The mean value (middle value)
+        :param std_dev: The scale of the Gaussian (standard dev)
+        :return: A random value drawn from the normal distribution
+        """
+        return np.random.normal(loc=mean, scale=std_dev)
+
+    def _uniform_mutation(self, lower=-1.0, upper=1.0):
+        """
+        Draws a real-valued number from a uniform distribution
+        :param lower: The lower bound for the distribution
+        :param upper: The upper bound for the distribution
+        :return: A random value drawn from the uniform distribution
+        """
+        return np.random.uniform(lower, upper)
 
 
 class Chromosome(ABC):
@@ -19,3 +56,5 @@ class Chromosome(ABC):
     this class.
     """
     pass
+
+
